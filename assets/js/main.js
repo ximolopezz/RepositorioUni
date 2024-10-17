84a379fd4ddaa28480f57c6a46efac1fda6e2163
alonmart5 <alonmart.dev@gmail.com>
2024-10-17 14:29:16 +0200

    eh

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,7 @@ function stopClock() {
     clearInterval(intervalId);
 }
 /*****************************************/
-
+/*
 window.onload = function () {
    
     console.log("Hello, world!");
@@ -31,4 +31,13 @@ function alonsoColor(elem){
     const g = Alonso(0, 255);
     const b = Alonso(0, 255);
     elem.style.color = "rgb("+r+","+g+","+b+")";
+}
+*/
+
+function cambia(elem){
+    elem.innerHTML = Math.random();
+}
+
+function cambia2(elem){
+    document.getElementsByTagName("main")[0].style.backgroundImage = "url('../img/Unknown.jpeg')";
 }
